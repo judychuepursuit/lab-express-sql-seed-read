508c13a676a5d57e72f093c720af1e0894263c3b
Judy <judychue@pursuit.org>
2023-07-25 00:10:33 -0400

    first B commit

--- back-end/controllers/songController.js
+++ back-end/controllers/songController.js
@@ -1,14 +1,31 @@
 const express = require("express");
 const songs = express.Router();
-const { getAllSongs } = require("../queries/songs");
+const { 
+  getAllSongs, 
+  getSong,
+  createSong,
+} = require("../queries/songs");
 
 // INDEX
-songs.get("/", async (req, res) => {
-  const allSongs = await getAllSongs();
-  if (allSongs[0]) {
-    res.status(200).json(allSongs);
+songs.get("/", async (req, res) => {});
+
+// SHOW
+songs.get("/:id", async (req, res) => {
+  const song = await getSong(id);
+  if (song) {
+    res.json(song);
   } else {
-    res.status(500).json({ error: "server error" });
+    res.status(404).json({ error: "not found" });
+  }
+});
+
+// CREATE
+songs.post("/", async (req, res) => {
+  try {
+    const song = await createSong(req.body);
+    res.json(song);
+  } catch (error) {
+    res.status(400).json({ error: error });
   }
 });
 
--- back-end/queries/songs.js
+++ back-end/queries/songs.js
@@ -2,11 +2,38 @@ const db = require("../db/dbConfig.js");
 
 const getAllSongs = async () => {
     try {
-        const allSongs = await db.any("SELECT * FROM songs");
-        return allSongs;
+      const allSongs = await db.any("SELECT * FROM songs");
+      return allSongs;
     } catch (error) {
       return error;
     }
 };
 
-module.exports = { getAllSongs };
+const getSong = async (id) => {
+    try {
+      const oneSong = await db.oneOrNone("SELECT * FROM songs WHERE id=$1", id);
+      return oneSong;
+    } catch (error) {
+      return error;
+    }
+};
+
+const createSong = async (song) => {
+  try {
+    const newSong = await db.one(
+      "INSERT INTO songs (name, artist, album, time, is_favorite) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+      [song.name, song.artist, song.album, song.time, song.is_favorite]
+    );
+    return newSong;
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = { 
+  getAllSongs,
+  getSong,
+  createSong,
+  // deleteSong, 
+  // updateSong
+};
